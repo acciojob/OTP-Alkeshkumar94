f3501d2d0f767fdb02643eddfa32b778afba7ba8
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-17 11:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function focusnext(currentinput){
+	const previnput=currentinput.previousElementSibling;
+	const nextinput=currentinput.nextElementSibling;
+	const maxlength=parseInt(currentinput.getAttribute('maxlength'));
+	if(currentinput.value.length===maxlength){
+		if(nextinput){
+			nextinput.focus();
+		}
+	}
+	else if(currentinput.value.length===maxlength)
+	{
+		if(previnput){
+			previnput.focus();
+		}
+	}
+	else if(currentinput.value.length===0)
+	{
+		if(previnput){
+			previnput.focus();
+		}
+	}
+}
