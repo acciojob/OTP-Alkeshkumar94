ac70f18600413c82353c8e803373201cb77aec05
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-18 12:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,10 @@ function focusnext(currentinput){
 	const previnput=currentinput.previousElementSibling;
 	const nextinput=currentinput.nextElementSibling;
 	const maxlength=parseInt(currentinput.getAttribute('maxlength'));
-	if(currentinput.value.length===maxlength){
-		if(nextinput){
-			nextinput.focus();
-		}
-	}
-	else if(currentinput.value.length===maxlength)
-	{
-		if(previnput){
-			previnput.focus();
-		}
-	}
-	else if(currentinput.value.length===0)
-	{
-		if(previnput){
-			previnput.focus();
-		}
-	}
+	
+	  if(currentinput.value.length === maxlength && nextinput){
+        nextinput.focus();
+    } else if(currentinput.value.length === 0 && previnput){
+        previnput.focus();
+    }
 }
-cy.get('.code').first().focus()  // This will focus on the first input field with class 'code'
-cy.focused().should('have.class', 'code')
