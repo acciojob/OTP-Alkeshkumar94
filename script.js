189e8fd30637ea0fa3113769f600a1a754f3fecd
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-18 17:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 //your JS code here. If required.
+
 function focusnext(currentinput){
 	const previnput=currentinput.previousElementSibling;
 	const nextinput=currentinput.nextElementSibling;
 	const maxlength=parseInt(currentinput.getAttribute('maxlength'));
+	if(currentinput.value.length===maxlength){
+		if(nextinput){
+			nextinput.focus();
+		}
+	}
+
+	else if(currentinput.value.length===0)
+	{
+		if(previnput){
+			previnput.focus();
+		}
+	}
 	
-	  if(currentinput.value.length === maxlength && nextinput){
-        nextinput.focus();
-    } else if(currentinput.value.length === 0 && previnput){
-        previnput.focus();
-    }
+    currentinput.addEventListener('keydown', function(event) {
+        if (event.key === 'Backspace' && currentinput.value.length === 0 && previnput) {
+            previnput.focus();
+        }
+    });
 }
